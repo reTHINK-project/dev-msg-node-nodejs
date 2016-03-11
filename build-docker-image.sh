81942e5b3af0217fbf5e442a9b1ae3d298949ccf
Nicolas DAVIAUD <nicolas.daviaud@apizee.com>
2016-03-11 13:27:25 +0100

    add docker instance builder

---
+++ build-docker-image.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+relPath=`dirname $0`
+cd $relPath
+
+if [ $# -ne 4 ]
+then
+    echo "\nUsage:\n$0 {image-name} {e.g. domain.tld} {e.g. 9090} {e.g. http://domain.tld}\n"
+    exit 1
+fi
+IMAGE=$1
+MSG_NODE_DOMAIN=$2
+MSG_NODE_PORT=$3
+DOMAIN_REGISTRY=$(echo "$4" | sed 's#/#\\/#g')
+
+sed -i "s/url:.*$/url: '${MSG_NODE_DOMAIN}',/g" src/configs/server-settings.js
+sed -i "s/domainRegistryUrl:.*$/domainRegistryUrl: '${DOMAIN_REGISTRY}',/g" src/configs/server-settings.js
+sed -i "s/port:.*$/port: '${MSG_NODE_PORT}',/g" src/configs/server-settings.js
+
+docker build -t $IMAGE .
